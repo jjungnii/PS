0e10d26e53468c22290b9ed3fb6a3a4ad96fea2d
JungEun Park <81218672+jjungnii@users.noreply.github.com>
2024-10-02 22:33:13 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 74.30 ms, Memory: 24.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -0,0 +1,7 @@
+def solution(participant, completion):
+    participant.sort()
+    completion.sort()
+    for i, person in enumerate(completion):
+        if person != participant[i]:
+            return participant[i]
+    return participant[-1]
