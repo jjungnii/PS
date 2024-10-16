d05e49834fedb8ead3473eb55db7d02459cf1078
JungEun Park <81218672+jjungnii@users.noreply.github.com>
2024-10-16 20:22:19 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 115.45 ms, Memory: 27.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -0,0 +1,9 @@
+def solution(arr):
+    answer = []
+    for num in arr:
+        if len(answer) == 0:
+            answer.append(num)
+        if answer[-1] != num:
+            answer.append(num)
+        
+    return answer
